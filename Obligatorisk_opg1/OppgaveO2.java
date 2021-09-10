1a3a88a43e66463df47813ea6e162e4ff31e7616
Westermin <reaverwester@gmail.com>
2021-09-10 18:25:17 +0200

    Update OppgaveO2.java

--- Obligatorisk_opg1/OppgaveO2.java
+++ Obligatorisk_opg1/OppgaveO2.java
@@ -1,25 +1,21 @@
-import java.util.Scanner;
+import static javax.swing.JOptionPane.showInputDialog;
 public class OppgaveO2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int p=-1; char k= '\0';
-        for (int i=0; i<10; i++){
-        System.out.println("Skriv inn poengsummen for å få utgitt karakter");
-        
-        while(p > 100 || p < 0) {
-            try{
-                p = sc.nextInt();}
-            catch(Exception e){
-                System.out.println("Ugyldig poengsum, skriv inn på nytt");
-                sc.next();}
+        int input;
+        for (int i=0; i < 10; i++) {
+            input = Integer.parseInt(showInputDialog("Hvor mange poeng fikk du?"));
+            if(input <= 0 || input > 100){                                             // ugyldig poengsum
+                System.out.println("Du har gitt ugyldig poengsum");
+                i--;
             }
-        if      (p - 89 > 0)k = 'A';
-        else if (p - 79 > 0)k = 'B';
-        else if (p - 59 > 0)k = 'C';
-        else if (p - 49 > 0)k = 'D';
-        else if (p - 39 > 0)k = 'E';
-        else                k = 'F';
-        System.out.println("Karakteren er: " + k);
+            else if (input > 0 && input <= 39)  System.out.println("Du har fått karakteren F");   // Karakter F
+            else if (input > 40 && input <= 49) System.out.println("Du har fått karakteren E");  // Karakter E
+            else if (input > 50 && input <=59)  System.out.println("Du har fått karakteren D"); // Karakter D
+            else if (input > 60 && input <=79)  System.out.println("Du har fått karakteren C");  // Karakter C
+            else if (input > 80 && input <=89)  System.out.println("Du har fått karakteren B");  // Karakter B
+            else if (input > 90 && input <=100) System.out.println("Du har fått karakteren A");  // Karakter A
+            else if (input > 100) System.out.println("Du har gitt ugyldig poengsum");
+
         }
     }
 }
