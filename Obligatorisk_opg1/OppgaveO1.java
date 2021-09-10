63485b60c73b2d3f21c82b009add66c02d77d9cb
Westermin <reaverwester@gmail.com>
2021-09-10 18:28:09 +0200

    Update OppgaveO1.java

--- Obligatorisk_opg1/OppgaveO1.java
+++ Obligatorisk_opg1/OppgaveO1.java
@@ -6,10 +6,10 @@ public class OppgaveO1 {
             System.out.println("Du har for lite inntekt for å skatte");
             System.exit(1);
         }
-        else if (input > 164100 && input <= 230950) input *= 0.0093;   // 0,93%
-        else if (input > 230950 && input <=580650)  input *= 0.0241;   // 2,41%
-        else if (input > 580650 && input <=934050)  input *= 0.01152;  // 11,52%
-        else if (input >=934050)                    input *= 0.01452;  // 14,52%
+        else if (input > 164100 && input <= 230950) input *= 0.0093;   // 0,93%  Trinn 1
+        else if (input > 230950 && input <=580650)  input *= 0.0241;   // 2,41%  Trinn 2
+        else if (input > 580650 && input <=934050)  input *= 0.01152;  // 11,52% Trinn 3
+        else if (input >=934050)                    input *= 0.01452;  // 14,52% Trinn 4
 
 
         System.out.println("Du er nødt til å skatte " + input + " NOK");
